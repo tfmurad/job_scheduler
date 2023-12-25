da122a0d7e4baa9e4c5dc8a791a35748602eccda
Al Murad Uzzaman <murad.themefisher@gmail.com>
2023-12-25 15:02:33 +0600

    welcome back fn added

--- server/server.js
+++ server/server.js
@@ -8,50 +8,41 @@ const PORT = 9000;
 app.use(express.json());
 app.use(cors());
 
-
-// app.post('/api/subscribe', async (req, res) => {
-//     try {
-//       const { name, email } = req.body;
-  
-//       const newUser = new User({ name, email, jobs: [] });
-  
-//       await agenda.start();
-//       const job1 = await agenda.schedule('in 2 seconds', 'send message', { userId: newUser._id, message: 'First message' });
-//       const job2 = await agenda.schedule('in 4 seconds', 'send message', { userId: newUser._id, message: 'Second message' });
-//       const job3 = await agenda.schedule('in 10 seconds', 'send message', { userId: newUser._id, message: 'Third message' });
-  
-//       newUser.jobs.push(job1.attrs._id, job2.attrs._id, job3.attrs._id);
-//       await newUser.save();
-  
-//       res.status(201).json({ message: `Welcome ${name}!!` });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send('Error subscribing user');
-//     }
-//   });
-
 app.post('/api/subscribe', async (req, res) => {
   try {
     const { name, email } = req.body;
 
-    // Check if the email already exists in the database
-    const existingUser = await User.findOne({ email });
+    let user = await User.findOne({ email });
 
-    if (existingUser) {
-      return res.status(400).json({ message: 'Email already subscribed' });
+    if (user) {
+      if (user.jobs.length === 0) {
+        await agenda.start();
+        const job1 = await agenda.schedule('in 2 seconds', 'send message', { userId: user._id, message: 'First message' });
+        const job2 = await agenda.schedule('in 4 seconds', 'send message', { userId: user._id, message: 'Second message' });
+        const job3 = await agenda.schedule('in 10 seconds', 'send message', { userId: user._id, message: 'Third message' });
+
+        user.jobs.push(job1.attrs._id, job2.attrs._id, job3.attrs._id);
+        await user.save();
+
+        return res.status(200).json({ message: `Welcome back ${name}!` });
+      } else {
+        // user exists and has jobs
+        return res.status(400).json({ message: 'Email already subscribed' });
+      }
+    } else {
+      // create new user and schedule jobs
+      const newUser = new User({ name, email, jobs: [] });
+
+      await agenda.start();
+      const job1 = await agenda.schedule('in 2 seconds', 'send message', { userId: newUser._id, message: 'First message' });
+      const job2 = await agenda.schedule('in 4 seconds', 'send message', { userId: newUser._id, message: 'Second message' });
+      const job3 = await agenda.schedule('in 10 seconds', 'send message', { userId: newUser._id, message: 'Third message' });
+
+      newUser.jobs.push(job1.attrs._id, job2.attrs._id, job3.attrs._id);
+      await newUser.save();
+
+      return res.status(201).json({ message: `Welcome ${name}!!` });
     }
-
-    const newUser = new User({ name, email, jobs: [] });
-
-    await agenda.start();
-    const job1 = await agenda.schedule('in 2 seconds', 'send message', { userId: newUser._id, message: 'First message' });
-    const job2 = await agenda.schedule('in 4 seconds', 'send message', { userId: newUser._id, message: 'Second message' });
-    const job3 = await agenda.schedule('in 10 seconds', 'send message', { userId: newUser._id, message: 'Third message' });
-
-    newUser.jobs.push(job1.attrs._id, job2.attrs._id, job3.attrs._id);
-    await newUser.save();
-
-    res.status(201).json({ message: `Welcome ${name}!!` });
   } catch (error) {
     // console.error(error);
     res.status(500).send('Error subscribing user');
@@ -61,7 +52,6 @@ app.post('/api/subscribe', async (req, res) => {
   
 app.post('/api/unsubscribe', async (req, res) => {
     const { userEmail } = req.body;
-    console.log(userEmail)
 
     try {
         const user = await User.findOne({ email: userEmail });
